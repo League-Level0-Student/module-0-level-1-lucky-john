fd1d9156a1c44ffa257958958a7a2b36c524cb57
lucky-john <league@Mac-mini-3.local>
2017-10-26 18:27:35 -0700

    Unbirthday,Remarkable(IP)

---
+++ src/Remarkable.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class Remarkable {
+
+public static void main(String[] args) {
+	
+}
+String remarkable = JOptionPane.
+
+if(remarkable){}
+}
---
+++ src/Unbirthday.java
@@ -0,0 +1,28 @@
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+
+public static void main(String[] args) {
+	String unbirthday = JOptionPane.showInputDialog("when is your birth day (day and month is required) ");
+	if (unbirthday.equals("10/26")) {
+		JOptionPane.showMessageDialog(null, "HAPPY BIRTHDAY ");
+		
+		
+		
+		
+	}
+	else{unbirthday.equals("somthing else");
+	
+	JOptionPane.showMessageDialog(null, "HAPPY UNBIRTHDAY");
+	}
+	
+	
+	
+}
+}
+
+
+
+	
+
+  

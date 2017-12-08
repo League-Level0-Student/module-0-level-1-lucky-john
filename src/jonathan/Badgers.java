85af049d3550cd052bdafcaf6c3be7290f696313
OrigamiMM <league@Mac-mini-3.local>
2017-12-07 17:55:55 -0800

    my ages and badgers

---
+++ src/jonathan/Badgers.java
@@ -0,0 +1,25 @@
+package jonathan;
+
+public class Badgers {
+public static void main(String[] args) {
+	for(int verse=1;verse<4;verse += 1){
+		System.out.println();
+	
+	for(int badger= 1;badger<11;badger += 1){
+		System.out.print("badger, ");
+		}
+	System.out.println();
+	for(int mushroom=1;mushroom<3;mushroom += 1){
+		System.out.print("mushroom ,");
+		
+		
+		
+		
+		
+	}
+	for(int a=1;a<2;a += 1){
+		System.out.print("a");
+}
+	}
+}
+}
---
+++ src/jonathan/MyAges.java
@@ -0,0 +1,10 @@
+package jonathan;
+
+public class MyAges {
+	public static void main(String[] args) {
+		for(int ages= 0;ages<15;ages += 1){
+			System.out.println(ages);
+		}
+	} 
+
+}
